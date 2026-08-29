16ad3ba38df32e15895b99545500917161900c06
vldezz <rbumiizz1122@gmail.com>
2026-08-29 19:33:20 +0700

    Add files via upload

---
+++ main.js
@@ -0,0 +1,40 @@
+/* TOGGLE SHARE */
+const shareBtn = document.getElementById("shareBtn");
+const shareLinks = document.getElementById("shareLinks");
+
+shareBtn.onclick = () => {
+    window.open("https://www.facebook.com/share/g/1HiiRCSSKd/", "_blank")
+};
+/* UNDANGAN */
+function joinWhatsAppGroup() {
+    window.open("https://whatsapp.com/channel/0029VbCKzuEFXUugctLPPy1D");
+}
+
+function openFacebookPage() {
+    window.open("https://www.facebook.com/share/g/1HiiRCSSKd/");
+}
+const video = document.getElementById("video");
+const overlay = document.getElementById("videoOverlay");
+
+let overlayClicked = false; 
+
+// Overlay muncul di detik tertentu
+video.addEventListener("timeupdate", () => {
+    if (video.currentTime >= 1 && !overlayClicked) {
+        overlay.classList.add("show");
+    }
+});
+
+// Klik overlay
+overlay.addEventListener("click", () => {
+    overlayClicked = true;              
+    overlay.style.display = "none";   
+    overlay.classList.remove("show");
+
+    // Aksi setelah klik
+    window.open("https://s.shopee.co.id/9V0JRZkOhT", "_blank");
+});
+
+
+
+
